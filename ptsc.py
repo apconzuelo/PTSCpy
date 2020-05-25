8cb376b06d42f38f8be8a65ed4d68151115345c0
Andrés Peña Conzuelo <andres.conzuelo@outlook.com>
2020-05-25 13:47:22 -0500

    update

--- ptsc.py
+++ ptsc.py
@@ -1,7 +1,7 @@
-class NullScreenTest(parameters = list()):
+class NullScreenTest():
 	
 	
-	def __init__(self,parameters):
+	def __init__(self, parameters = []):
 		self.par = parameters
 	
 	def create_ideal_pattern(self):
