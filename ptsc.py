bb3ce3d441ea7a2804a2f655927d456a3f4779f2
Andrés Peña Conzuelo <andres.conzuelo@outlook.com>
2020-05-25 12:47:09 -0500

    Regularupdate

---
+++ ptsc.py
@@ -0,0 +1,16 @@
+class NullScreenTest(parameters):
+	
+	
+	def __init__(self,parameters):
+		self.par = parameters
+	
+	def create_ideal_pattern(self):
+		x = np.linspace(0, 1, par.NumberOfPoints)*self.par.CCDx - self.par.CCDx/2
+		y = np.linspace(0, 1, par.NumberOfPoints)*self.par.CCDy - self.par.CCDy/2
+  
+  def generate_NS(self):
+  	self.p1 = self.create_ideal_pattern()
+  	self.p2 = self.project_to_surface()
+  	self.p3 = self.project_to_NullScreen()
+  	
+  	
