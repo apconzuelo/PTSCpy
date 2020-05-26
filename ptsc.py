c28bb20300435e043380cdd9acdd1b4d29abf492
Andrés Peña Conzuelo <andres.conzuelo@outlook.com>
2020-05-26 11:26:07 -0500

    up

--- ptsc.py
+++ ptsc.py
@@ -1,9 +1,27 @@
 import numpy as np
 #import pandas as pd
 
+class CriticalRayPoint():
+	
+	def __init__(self, x=None, y=None, z=None):
+		if len(x) > 0:
+			self = np.vstack([x, y, z]).T
+		else
+			raise('Cannot create CriticalRayPoint, input inexistent')
+			
+	def separate(self):
+		x = self[:, 0]
+		y = self[:, 1]
+		z = self[:, 2]
+		return x, y, z
+	
+	
+
+	
+	
+	
 class NullScreenTest():
-	
-	
+		
 	def __init__(self, parameters = []):
 		self.par = parameters
 		self.p1 = self.create_ideal_pattern()
