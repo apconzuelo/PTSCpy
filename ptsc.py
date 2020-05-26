c96e3a1a51b8017050ea1295f8159b7c9ec2d616
Andrés Peña Conzuelo <andres.conzuelo@outlook.com>
2020-05-25 19:06:11 -0500

    update

--- ptsc.py
+++ ptsc.py
@@ -9,13 +9,15 @@ class NullScreenTest():
 	
 	def create_ideal_pattern(self):
 		p1 = np.ones([3, 500])
+		'''
 		p1[0‚ :] = np.linspace(-1, 1, 500)*self.par.CCDX.values[0] 
 		p1[1, :] = np.linspace(-1, 1, 500)*self.par.CCDY.values[0]
-		'''
+		
 		p1[0, :] = np.linspace(0, 1, 500)*5
 		p1[1, :] = np.linspace(0, 5, 500)
-		'''
+		
 		p1[2, :] = p1[2, :]*(self.par.FocalLength.values[0] + self.par.b.values[0])
+		'''
 		return p1
 
 	def generate_NS(self):
